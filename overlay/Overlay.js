f80c8b45b0317bb2c99065695c87f383e0da80fc
Tammo Pape <git@tammopape.de>
2018-04-03 15:22:11 +0200

    adds link to the screenshot (to make it usable on mobile)

--- overlay/Overlay.js
+++ overlay/Overlay.js
@@ -5,15 +5,42 @@ import Form from './Form'
 import Icon from './Icon'
 import styles from './overlay.css'
 
+const THREE_FRAMES = 17 * 3
+
 export class Overlay extends React.Component {
   state = {
+    hide: true,
     open: false,
   }
 
+  togglOpen = cb => {
+    this.setState(
+      {
+        open: !this.state.open,
+      },
+      cb,
+    )
+  }
+
+  togglHide = cb => {
+    this.setState(
+      {
+        hide: !this.state.hide,
+      },
+      cb,
+    )
+  }
+
   toggl = () => {
-    this.setState({
-      open: !this.state.open,
-    })
+    if (this.state.hide) {
+      this.togglHide(() => {
+        setTimeout(this.togglOpen, THREE_FRAMES)
+      })
+    } else {
+      this.togglOpen(() => {
+        setTimeout(this.togglHide, THREE_FRAMES)
+      })
+    }
   }
 
   render() {
@@ -23,7 +50,7 @@ export class Overlay extends React.Component {
           <Icon />
         </div>
 
-        <div className={cn(styles.box, this.state.open ? styles.active : undefined)}>
+        <div className={cn(styles.box, this.state.open ? styles.active : undefined, this.state.hide ? styles.hide : undefined)}>
           <div className={styles.header} />
           <div className={styles.content}>
             <span onClick={this.toggl} className={styles.close}>
